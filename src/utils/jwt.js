bc2ac689972e9fcfd0c3d125ccc47e4b215ad224
desmondemmanuel020 <desmondemmanuel020@gmail.com>
2026-04-24 08:37:40 +0100

    Delete src directory

--- src/utils/jwt.js
+++
@@ -1,4 +0,0 @@
-const jwt = require('jsonwebtoken');
-const sign   = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN || '30d' });
-const verify = (token) => jwt.verify(token, process.env.JWT_SECRET);
-module.exports = { sign, verify };
